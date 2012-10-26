8406ce33a3d23fa9bcb2fefcb758b8f05fb938cd
Brian Avery <@>
2012-10-26 14:27:08 -0400

    fixed dependencies

--- Android.mk
+++ Android.mk
@@ -2,7 +2,7 @@ LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES:= \
-	qtklaatu-qmlscene-dependencyForcer.c
+	qtklaatu-demos-dependencyForcer.c
 
 #LOCAL_SHARED_LIBRARIES := \
 #	libutils \
@@ -14,17 +14,22 @@ ifeq ($(TARGET_OS),linux)
 #LOCAL_SHARED_LIBRARIES += librt
 endif
 
-LOCAL_MODULE:= qtklaatu-qmlscene-dependencyForcer
+LOCAL_MODULE:= qtklaatu-demos-dependencyForcer
 LOCAL_MODULE_TAGS := eng
 LOCAL_MODULE_CLASS := EXECUTABLES
-
+# this line makes sure we build AFTER the base Qt libraries
+LOCAL_ADDITIONAL_DEPENDENCIES := $(TARGET_OUT_EXECUTABLES)/qtbase-dependencyForcer 
+LOCAL_ADDITIONAL_DEPENDENCIES += $(TARGET_OUT_EXECUTABLES)/qtjsbackend-dependencyForcer
+LOCAL_ADDITIONAL_DEPENDENCIES += $(TARGET_OUT_EXECUTABLES)/qtdeclarative-dependencyForcer
 intermediates:= $(local-intermediates-dir)
 
-GEN := $(LOCAL_PATH)/qtklaatu-qmlscene-dependencyForcer.c
+GEN := $(LOCAL_PATH)/qtklaatu-demos-dependencyForcer.c
 $(GEN): PRIVATE_INPUT_FILE := $(LOCAL_PATH)/AndroidQtBuild.sh
-$(GEN): PRIVATE_CUSTOM_TOOL = bash $(PRIVATE_INPUT_FILE) $@ $(LOCAL_PATH)
+$(GEN): PRIVATE_CUSTOM_TOOL = bash $(PRIVATE_INPUT_FILE) $@ 
 $(GEN): $(LOCAL_PATH)/AndroidQtBuild.sh 
 	$(transform-generated-source)
+$(GEN):$(TARGET_OUT_EXECUTABLES)/qtdeclarative-dependencyForcer 
+.PHONY: $(GEN)
 #LOCAL_GENERATED_SOURCES += $(GEN)
 
 
