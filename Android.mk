3c9ff1b6a6b22ea4eb00b0fe1d815c9e24f181fc
Greg Bentley <@>
2013-10-23 16:12:08 -0700

    Split init.klaatu.rc into separate files.

--- Android.mk
+++ Android.mk
@@ -1,8 +1,8 @@
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
-file := $(TARGET_ROOT_OUT)/default.prop
-$(file) : $(LOCAL_PATH)/init/default.prop | $(ACP)
+file := $(TARGET_ROOT_OUT)/init.klaatu-common.rc
+$(file) : $(LOCAL_PATH)/init/init.klaatu-common.rc | $(ACP)
 	$(transform-prebuilt-to-target)
 ALL_MODULES += $(file)
 ALL_DEFAULT_INSTALLED_MODULES += $(file)
@@ -19,7 +19,7 @@ $(file) : $(LOCAL_PATH)/init/fixInitRc | $(TARGET_ROOT_OUT)/init.rc
 ALL_MODULES += $(file)
 ALL_DEFAULT_INSTALLED_MODULES += $(file)
 
-file := $(TARGET_OUT)/hostname_init.sh
+file := $(TARGET_OUT_EXECUTABLES)/hostname_init.sh
 $(file) : $(LOCAL_PATH)/scripts/hostname_init.sh | $(ACP)
 	$(transform-prebuilt-to-target)
 ALL_MODULES += $(file)
